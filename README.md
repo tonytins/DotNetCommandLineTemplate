907d9c65d76886d2c236e1390292c61910aee416
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-10-08 03:43:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# System.CommandLine Template
+
+**WORK IN PROGRESS!**
+
+This is a basic ``dotnet new`` template for creating command line application using the [System.CommandLine](hhttps://learn.microsoft.com/en-us/dotnet/standard/commandline/).
+
+## License
+
+I license this project under the MIT license - see [LICENSE](LICENSE) for details.
---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿var fileOpt = new Option<FileInfo?>(
+    name: "--file",
+    description: "The file to read and display on the terminal.");
+
+var rootCmd = new RootCommand("Your simple command line app");
+rootCmd.AddOption(fileOpt);
+
+/// <summary>
+/// 
+/// </summary>
+/// <value></value>
+rootCmd.SetHandler((file) =>
+{
+    ReadFile(file!);
+
+}, fileOpt);
+
+await rootCmd.InvokeAsync(args);
+
+void ReadFile(FileInfo file)
+{
+    File.ReadLines(file.FullName)
+        .ToList()
+        .ForEach(line => Console.WriteLine(line));
+}
